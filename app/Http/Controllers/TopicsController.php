5252301dbf96e3bdbdadc15288f959721f3339e5
phpdance <1780997282@qq.com>
2018-03-26 02:26:27 +0000

    Notify user in email with Queue supported

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,7 +7,6 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
-use App\Models\User;
 use Auth;
 use App\Handlers\ImageUploadHandler;
 
