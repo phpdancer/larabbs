92759fc9e6d96393a09ba55e11a64ec988844419
phpdance <1780997282@qq.com>
2018-03-29 12:03:51 +0000

    Sidebar links

---
+++ app/Observers/LinkObserver.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: friday.zhu
+ * Date: 2018/3/29
+ * Time: 19:59
+ */
+namespace App\Observers;
+
+use App\Models\Link;
+use Cache;
+
+class LinkObserver
+{
+    // 在保存时清空 cache_key 对应的缓存
+    public function saved(Link $link)
+    {
+        Cache::forget($link->cache_key);
+    }
+}
